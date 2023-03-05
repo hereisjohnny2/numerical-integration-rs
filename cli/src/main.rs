1de409d13c4bd0d45562a590643cfc01018d9c8e
joao.carvalho <joao.carvalho@intelie.com.br>
2023-03-05 12:30:30 -0300

    refactor: use n degree polynomial in cli application

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,22 +3,25 @@ use std::io;
 use numerical_integration_core::{functions::polynomial::Polynomial, integrals::trapezoid, utils};
 
 fn main() -> Result<(), io::Error> {
-    println!("Enter the integration parameters");
+    println!("--- Integration Parameters ---");
     let n = utils::read_number::<i32>("Number of iterations:")?;
     let lower_bound = utils::read_number::<f64>("Lower bound:")?;
     let upper_bound = utils::read_number::<f64>("Upper bound:")?;
 
-    println!("Enter the paramters of the quadratic function (f(x) = c0 + c1*x + c2*x²)");
-    let c0 = utils::read_number::<f64>("c0:")?;
-    let c1 = utils::read_number::<f64>("c1:")?;
-    let c2 = utils::read_number::<f64>("c2:")?;
+    println!("\n--- Function Coeficients ---");
+    let degree = utils::read_number::<i32>("Enter polynomial function degree: ")?;
+    let mut coefficients: Vec<f64> = Vec::new();
+
+    for i in 0..(degree + 1) {
+        let coeff = utils::read_number::<f64>(&format!("c{}:", i).to_string())?;
+        coefficients.push(coeff);
+    }
 
     println!(
         "n = {}, lower = {}, upper = {}",
         n, lower_bound, upper_bound
     );
 
-    let coefficients = vec![c0, c1, c2];
     let poly_2 = Polynomial::new(coefficients);
     println!("{}", poly_2.to_string());
 
