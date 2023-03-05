04233ebad830d0d6771bb6a10d29dbed4324a0d7
joao.carvalho <joao.carvalho@intelie.com.br>
2023-03-05 11:26:19 -0300

    feat: add function to express poly function as string

--- numerical_integration_core/src/functions/polynomial.rs
+++ numerical_integration_core/src/functions/polynomial.rs
@@ -8,6 +8,11 @@ pub struct Polynomial {
 impl Polynomial {
     pub fn new(coefficients: Vec<f64>) -> Self {
         let degree = coefficients.len();
+
+        if degree < 1 {
+            panic!("it's not possible to create a polynomial degree equals to 0");
+        }
+
         Polynomial {
             coefficients,
             degree,
@@ -16,6 +21,27 @@ impl Polynomial {
 
     pub fn degree(&self) -> usize {
         self.degree
+    }
+
+    pub fn expr(&self) -> String {
+        let mut expression = String::from(format!("f(x) = {}", self.coefficients.get(0).unwrap()));
+        for i in 1..self.degree {
+            let coeff = self.coefficients.get(i).unwrap();
+
+            if coeff < &0.0 {
+                expression.push_str(format!(" - {}", coeff).as_str());
+            } else {
+                expression.push_str(format!(" + {}", coeff).as_str());
+            }
+
+            if i == 1 {
+                expression.push_str(format!(" * x").as_str());
+            } else if i > 1 {
+                expression.push_str(format!(" * x^{}", i).as_str());
+            }
+        }
+
+        expression
     }
 }
 
@@ -51,5 +77,35 @@ mod tests {
         let result = poly.f(10.0);
 
         assert_eq!(expected, result);
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_panic_if_coeffs_len_is_zero() {
+        Polynomial::new(vec![]);
+    }
+
+    #[test]
+    fn should_get_expression() {
+        let coefficients_0 = vec![1.0];
+        let poly_0 = Polynomial::new(coefficients_0);
+        let expected_0 = "f(x) = 1";
+        let result_0 = poly_0.expr();
+
+        assert_eq!(expected_0, result_0);
+
+        let coefficients_1 = vec![1.0, 2.0];
+        let poly_1 = Polynomial::new(coefficients_1);
+        let expected_1 = "f(x) = 1 + 2 * x";
+        let result_1 = poly_1.expr();
+
+        assert_eq!(expected_1, result_1);
+
+        let coefficients_4 = vec![1.0, 2.0, 3.0, 4.0];
+        let poly_4 = Polynomial::new(coefficients_4);
+        let expected_4 = "f(x) = 1 + 2 * x + 3 * x^2 + 4 * x^3";
+        let result_4 = poly_4.expr();
+
+        assert_eq!(expected_4, result_4);
     }
 }
